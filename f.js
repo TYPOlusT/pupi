eed2125fd6e769311616b7c7800233a0a4b8118a
Kizilbaev Tamerlan <yushina-5@mail.ru>
2023-10-12 15:49:03 +0500

    64644

---
+++ f.js
@@ -0,0 +1,9 @@
+document.write("<h1>Зимнее утро</h1>");
+document.write("<h3>Александр Сергеевич Пушкин</h3>");
+document.write("<br>Мороз и солнце; день чудесный!</br>");
+document.write("<br>Еще ты дремлешь, друг прелестный —</br>");
+document.write("<br>Пора, красавица, проснись:</br>");
+document.write("<br>Открой сомкнуты негой взоры</br>");
+document.write("<br>Навстречу северной Авроры,</br>");
+document.write("<br>Звездою севера явись!</br>");
+document.write("<br>...</br>");
